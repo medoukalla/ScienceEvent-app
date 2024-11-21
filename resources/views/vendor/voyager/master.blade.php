75f853662e7ff3fefcd0e9ccc8e79e84c95120a1
Mohamed Oukalla <medoukalla@gmail.com>
2024-11-21 22:20:48 +0000

    Add assets of new dashboard design

--- resources/views/vendor/voyager/master.blade.php
+++ resources/views/vendor/voyager/master.blade.php
@@ -1,14 +1,22 @@
 <!DOCTYPE html>
-<html lang="{{ config('app.locale') }}" dir="{{ __('voyager::generic.is_rtl') == 'true' ? 'rtl' : 'ltr' }}">
-<head>
+
+<html lang="{{ config('app.locale') }}" dir="{{ __('voyager::generic.is_rtl') == 'true' ? 'rtl' : 'ltr' }}" class="light-style layout-navbar-fixed layout-menu-fixed layout-compact " data-theme="theme-default" data-assets-path="assets/" data-template="vertical-menu-template" data-style="night">
+
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
     <title>@yield('page_title', setting('admin.title') . " - " . setting('admin.description'))</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
-    <meta name="assets-path" content="{{ route('voyager.voyager_assets') }}"/>
 
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
-
+    
+    <meta name="description" content="Start your development with a Dashboard for Bootstrap 5" />
+    <meta name="keywords" content="dashboard, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5">
+    <!-- Canonical SEO -->
+    <link rel="canonical" href="/">
+    
+    
     <!-- Favicon -->
     <?php $admin_favicon = Voyager::setting('admin.icon_image', ''); ?>
     @if($admin_favicon == '')
@@ -17,134 +25,174 @@
         <link rel="shortcut icon" href="{{ Voyager::image($admin_favicon) }}" type="image/png">
     @endif
 
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap" rel="stylesheet">
+
+    <!-- Icons -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/fontawesome.css') }}" />
+    {{-- <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/tabler-icons.css') }}"/> --}}
+    <link href="https://cdn.jsdelivr.net/npm/@tabler/icons@3.4.0/font/tabler-icons.min.css" rel="stylesheet"/>
 
 
-    <!-- App CSS -->
-    <link rel="stylesheet" href="{{ voyager_asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/fonts/flag-icons.css') }}" />
 
-    @yield('css')
-    @if(__('voyager::generic.is_rtl') == 'true')
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-rtl/3.4.0/css/bootstrap-rtl.css">
-        <link rel="stylesheet" href="{{ voyager_asset('css/rtl.css') }}">
-    @endif
+    <!-- Core CSS -->
+    
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/core.css') }}" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/rtl/theme-default.css') }}" class="template-customizer-theme-css" />
+    
+    <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}" />
+    
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/node-waves/node-waves.css') }}" />
+    
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/typeahead-js/typeahead.css') }}" /> 
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/apex-charts/apex-charts.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/swiper/swiper.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-bs5/datatables.bootstrap5.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.css') }}">
 
-    <!-- Few Dynamic Styles -->
-    <style type="text/css">
-        .voyager .side-menu .navbar-header {
-            background:{{ config('voyager.primary_color','#22A7F0') }};
-            border-color:{{ config('voyager.primary_color','#22A7F0') }};
+    <!-- Page CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/cards-advance.css') }}" />
+
+    
+    
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="{{ asset('assets/js/config.js') }}"></script>
+    
+  </head>
+
+  <body class="voyager @if(isset($dataType) && isset($dataType->slug)){{ $dataType->slug }}@endif">
+
+    @php
+        if (\Illuminate\Support\Str::startsWith(Auth::user()->avatar, 'http://') || \Illuminate\Support\Str::startsWith(Auth::user()->avatar, 'https://')) {
+        $user_avatar = Auth::user()->avatar;
+        } else {
+            $user_avatar = Voyager::image(Auth::user()->avatar);
         }
-        .widget .btn-primary{
-            border-color:{{ config('voyager.primary_color','#22A7F0') }};
-        }
-        .widget .btn-primary:focus, .widget .btn-primary:hover, .widget .btn-primary:active, .widget .btn-primary.active, .widget .btn-primary:active:focus{
-            background:{{ config('voyager.primary_color','#22A7F0') }};
-        }
-        .voyager .breadcrumb a{
-            color:{{ config('voyager.primary_color','#22A7F0') }};
-        }
-    </style>
+    @endphp
 
-    @if(!empty(config('voyager.additional_css')))<!-- Additional CSS -->
-        @foreach(config('voyager.additional_css') as $css)<link rel="stylesheet" type="text/css" href="{{ asset($css) }}">@endforeach
-    @endif
+    
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar  ">
+        <div class="layout-container">
 
-    @yield('head')
-</head>
+            <!-- Menu Sidebar -->
+            @include('voyager::dashboard.sidebar')
 
-<body class="voyager @if(isset($dataType) && isset($dataType->slug)){{ $dataType->slug }}@endif">
+            <!-- Menu Sidebar -->
 
-<div id="voyager-loader">
-    <?php $admin_loader_img = Voyager::setting('admin.loader', ''); ?>
-    @if($admin_loader_img == '')
-        <img src="{{ voyager_asset('images/logo-icon.png') }}" alt="Voyager Loader">
-    @else
-        <img src="{{ Voyager::image($admin_loader_img) }}" alt="Voyager Loader">
-    @endif
-</div>
+            <!-- Layout container -->
+            <div class="layout-page">
 
-<?php
-if (\Illuminate\Support\Str::startsWith(Auth::user()->avatar, 'http://') || \Illuminate\Support\Str::startsWith(Auth::user()->avatar, 'https://')) {
-    $user_avatar = Auth::user()->avatar;
-} else {
-    $user_avatar = Voyager::image(Auth::user()->avatar);
-}
-?>
+                <!-- Navbar -->
+                @include('voyager::dashboard.navbar')
+                <!-- Navbar -->
 
-<div class="app-container">
-    <div class="fadetoblack visible-xs"></div>
-    <div class="row content-container">
-        @include('voyager::dashboard.navbar')
-        @include('voyager::dashboard.sidebar')
-        <script>
-            (function(){
-                    var appContainer = document.querySelector('.app-container'),
-                        sidebar = appContainer.querySelector('.side-menu'),
-                        navbar = appContainer.querySelector('nav.navbar.navbar-top'),
-                        loader = document.getElementById('voyager-loader'),
-                        hamburgerMenu = document.querySelector('.hamburger'),
-                        sidebarTransition = sidebar.style.transition,
-                        navbarTransition = navbar.style.transition,
-                        containerTransition = appContainer.style.transition;
 
-                    sidebar.style.WebkitTransition = sidebar.style.MozTransition = sidebar.style.transition =
-                    appContainer.style.WebkitTransition = appContainer.style.MozTransition = appContainer.style.transition =
-                    navbar.style.WebkitTransition = navbar.style.MozTransition = navbar.style.transition = 'none';
+                <!-- Content wrapper -->
+                <div class="content-wrapper vyager">
 
-                    if (window.innerWidth > 768 && window.localStorage && window.localStorage['voyager.stickySidebar'] == 'true') {
-                        appContainer.className += ' expanded no-animation';
-                        loader.style.left = (sidebar.clientWidth/2)+'px';
-                        hamburgerMenu.className += ' is-active no-animation';
-                    }
+                    <!-- Content -->
+                    <div class="container-xxl flex-grow-1 container-p-y">
+                        <div class="row g-6" id="vyager">
+                            @yield('content')
+                        </div>
+                    </div>
+                    <!-- Content -->
 
-                   navbar.style.WebkitTransition = navbar.style.MozTransition = navbar.style.transition = navbarTransition;
-                   sidebar.style.WebkitTransition = sidebar.style.MozTransition = sidebar.style.transition = sidebarTransition;
-                   appContainer.style.WebkitTransition = appContainer.style.MozTransition = appContainer.style.transition = containerTransition;
-            })();
-        </script>
-        <!-- Main Content -->
-        <div class="container-fluid">
-            <div class="side-body padding-top">
-                @yield('page_header')
-                <div id="voyager-notifications"></div>
-                @yield('content')
+
+                    <!-- Footer -->
+                    @include('voyager::partials.app-footer')
+                    <!-- Footer -->
+
+
+                    <div class="content-backdrop fade"></div>
+
+
+                </div>
+                <!-- Content wrapper -->
+
+
+
             </div>
+            <!-- / Layout page -->
+
+
         </div>
+
+
+        <!-- Overlay -->
+        <div class="layout-overlay layout-menu-toggle"></div>
+        <!-- Drag Target Area To SlideIn Menu On Small Screens -->
+        <div class="drag-target"></div>
     </div>
-</div>
-@include('voyager::partials.app-footer')
-
-<!-- Javascript Libs -->
+    <!-- / Layout wrapper -->
 
 
-<script type="text/javascript" src="{{ voyager_asset('js/app.js') }}"></script>
+        
 
-<script>
-    @if(Session::has('alerts'))
-        let alerts = {!! json_encode(Session::get('alerts')) !!};
-        helpers.displayAlerts(alerts, toastr);
+
+
+    <!-- Javascript Libs -->
+    <!-- Core JS -->
+    <script src="{{ asset('assets/vendor/libs/jquery/jquery.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/popper/popper.js') }}"></script>
+    <script src="{{ asset('assets/vendor/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/node-waves/node-waves.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/hammer/hammer.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/i18n/i18n.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/typeahead-js/typeahead.js') }}"></script>
+    <script src="{{ asset('assets/vendor/js/menu.js') }}"></script>
+    
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+    <script src="{{ asset('assets/vendor/libs/apex-charts/apexcharts.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/swiper/swiper.js') }}"></script>
+    <script src="{{ asset('assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js') }}"></script>
+
+    <!-- Main JS -->
+    <script src="{{ asset('assets/js/main.js') }}"></script>
+    
+
+    <!-- Page JS -->
+    <script src="{{ asset('assets/js/dashboards-analytics.js') }}"></script>
+
+
+    <script type="text/javascript" src="{{ voyager_asset('js/app.js') }}"></script>
+
+    <script>
+        @if(Session::has('alerts'))
+            let alerts = {!! json_encode(Session::get('alerts')) !!};
+            helpers.displayAlerts(alerts, toastr);
+        @endif
+
+        @if(Session::has('message'))
+
+        // TODO: change Controllers to use AlertsMessages trait... then remove this
+        var alertType = {!! json_encode(Session::get('alert-type', 'info')) !!};
+        var alertMessage = {!! json_encode(Session::get('message')) !!};
+        var alerter = toastr[alertType];
+
+        if (alerter) {
+            alerter(alertMessage);
+        } else {
+            toastr.error("toastr alert-type " + alertType + " is unknown");
+        }
+        @endif
+    </script>
+    @include('voyager::media.manager')
+    @yield('javascript')
+    @stack('javascript')
+    @if(!empty(config('voyager.additional_js')))<!-- Additional Javascript -->
+        @foreach(config('voyager.additional_js') as $js)<script type="text/javascript" src="{{ asset($js) }}"></script>@endforeach
     @endif
-
-    @if(Session::has('message'))
-
-    // TODO: change Controllers to use AlertsMessages trait... then remove this
-    var alertType = {!! json_encode(Session::get('alert-type', 'info')) !!};
-    var alertMessage = {!! json_encode(Session::get('message')) !!};
-    var alerter = toastr[alertType];
-
-    if (alerter) {
-        alerter(alertMessage);
-    } else {
-        toastr.error("toastr alert-type " + alertType + " is unknown");
-    }
-    @endif
-</script>
-@include('voyager::media.manager')
-@yield('javascript')
-@stack('javascript')
-@if(!empty(config('voyager.additional_js')))<!-- Additional Javascript -->
-    @foreach(config('voyager.additional_js') as $js)<script type="text/javascript" src="{{ asset($js) }}"></script>@endforeach
-@endif
 
 </body>
 </html>
--- resources/views/vendor/voyager/partials/app-footer.blade.php
+++ resources/views/vendor/voyager/partials/app-footer.blade.php
@@ -1,13 +1,23 @@
-<footer class="app-footer">
-    <div class="site-footer-right">
-        @if (rand(1,100) == 100)
-            <i class="voyager-rum-1"></i> {{ __('voyager::theme.footer_copyright2') }}
-        @else
-            {!! __('voyager::theme.footer_copyright') !!} <a href="http://thecontrolgroup.com" target="_blank">The Control Group</a>
-        @endif
-        @php $version = Voyager::getVersion(); @endphp
-        @if (!empty($version))
-            - {{ $version }}
-        @endif
+<footer class="content-footer footer bg-footer-theme">
+    <div class="container-xxl">
+      <div class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
+        <div class="text-body">
+          © <script>
+          document.write(new Date().getFullYear())
+
+          </script>, made with ❤️ by <a href="https://pixinvent.com" target="_blank" class="footer-link">Pixinvent</a>
+        </div>
+        <div class="d-none d-lg-inline-block">
+          
+          <a href="https://themeforest.net/licenses/standard" class="footer-link me-4" target="_blank">License</a>
+          <a href="https://1.envato.market/pixinvent_portfolio" target="_blank" class="footer-link me-4">More Themes</a>
+          
+          <a href="https://demos.pixinvent.com/vuexy-html-admin-template/documentation/" target="_blank" class="footer-link me-4">Documentation</a>
+          
+          
+          <a href="https://pixinvent.ticksy.com/" target="_blank" class="footer-link d-none d-sm-inline-block">Support</a>
+          
+        </div>
+      </div>
     </div>
 </footer>
