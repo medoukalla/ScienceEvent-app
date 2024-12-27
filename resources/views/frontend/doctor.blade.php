8559fd28fb4bd290f55c67b925b76a8a51579330
Mohamed Oukalla <medoukalla@gmail.com>
2024-12-27 11:40:59 +0000

    Switch doctor page from static to dunamique

--- resources/views/frontend/doctor.blade.php
+++ resources/views/frontend/doctor.blade.php
@@ -1,7 +1,6 @@
 @extends('template')
 
 @section('content')
-<script src="https://cdn.tailwindcss.com"></script>
 
 
 <div class="profile-bio section-padding-bottom-130">
@@ -11,54 +10,45 @@
                 <div data-w-id="316ed8d2-66d5-d21f-f414-3781cc3a9386"
                     style="-webkit-transform:translate3d(0, 70px, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 70px, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 70px, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 70px, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);opacity:1"
                     class="class-details-summary-wrap w-richtext">
-                    <h3>Dr Mathieu Bataille</h3>
-                    <p>Le Docteur Mathieu Bataille, Médecin dermatologue, praticien hospitalier à temps partiel sur le CHR de Saint Omer ainsi que praticien consultant à l’Hôpital de St Vincent Lille. Il est membre depuis 2016 de la société Française de Dermatologie et Vénéréologie. Il crée en 2019, le groupe de télé-dermatologie et e-santé de la SFD. Dr Bataille est détenteur de 5 DU, Dermatologie chirurgicale : Université de Versailles Saint-Quentin-en-Yvelines, 2016 - 2017 : Télémédecine :Faculté de Droit, Institut Catholique de Lille.  En 2016 - 2017 : Dermatologie pédiatrique : Faculté de Médecine, Université de Nantes. En 2015 - 2016 : Immunothérapies ciblées des maladies inflammatoires et auto-immunes. Faculté de Médecine de Lille H. Warembourg, Université de Lille II et en 2015 - 2016 : Dermatologie Infectieuse et Tropicale : Faculté de Médecine, Université Pierre et Marie Curie, Paris VI. Il est le co-investigateur de différentes recherches cliniques. Il enseigne à la faculté de médecine de Lille ainsi qu’en formation continue. Il est également l’auteur de diverses publications et articles internationaux et nationaux sur le champ de la dermatologie.</p>
+                    <h3>{{ $doctor->name }}</h3>
+                    <p>{{ $doctor->bio }}</p>
                     <div class="more-info-by-title">
-                        More information by <span>Dr Mathieu Bataille</span>
+                        Formation par : <span>{{ $doctor->name }}</span>
                     </div>
                     <div class="more-info-flex">
-                        <div class="more-info-wrap">
-                            <div class="more-info-img">
-                                <img src="{{ asset('assets/images/s-video-4.png') }}" alt="">
+                        @foreach ( $doctor->formations as $formation) 
+                            <div class="more-info-wrap">
+                                <div class="more-info-img">
+                                    @php
+                                        $videos = json_decode($formation->video);
+                                    @endphp
+                                    
+                                    @if (!empty($videos) && isset($videos[0]->download_link))
+                                        <a href="{{ route('frontend.formation', $formation->id) }}">
+                                            <video style="width: 100%; height: auto;" controlslist="nodownload nofullscreen noplaybackrate" disablepictureinpicture autoplay muted playsinline>
+                                                <source src="{{ asset('storage/' . $videos[0]->download_link) }}" type="video/mp4">
+                                                Votre navigateur ne prend pas en charge la balise vidéo.
+                                            </video>
+                                        </a>
+                                    @else
+                                        <p>Aucune vidéo disponible.</p>
+                                    @endif
+                                </div>
+                                <div class="more-info-title">
+                                    <a href="{{ route('frontend.formation', $formation->id) }}">
+                                        <span>{{ $formation->category->name }}</span>
+                                        <p>{{ $formation->title }}</p>
+                                    </a>
+                                </div>
+                                <div class="more-info-date">
+                                    <a href="{{ route('frontend.formation', $formation->id) }}">
+                                        <p>
+                                            {{ $formation->brief }} <span>à : {{ \Carbon\Carbon::parse($formation->created_at)->format('d F Y') }}</span>
+                                        </p>
+                                    </a>
+                                </div>
                             </div>
-                            <div class="more-info-title">
-                                <span>Dermatologie générale</span>
-                                <p>Démarche diagnostique des affections...</p>
-                            </div>
-                            <div class="more-info-date">
-                                <p>
-                                    Pour près de 20 000 professionnels de santé. <span>Mis à jour en 2024.</span>
-                                </p>
-                            </div>
-                        </div>
-                        <div class="more-info-wrap">
-                            <div class="more-info-img">
-                                <img src="{{ asset('assets/images/s-video-1.png') }}" alt="">
-                            </div>
-                            <div class="more-info-title">
-                                <span>Dermatologie générale</span>
-                                <p>Démarche diagnostique des affections...</p>
-                            </div>
-                            <div class="more-info-date">
-                                <p>
-                                    Pour près de 20 000 professionnels de santé. <span>Mis à jour en 2024.</span>
-                                </p>
-                            </div>
-                        </div>
-                        <div class="more-info-wrap">
-                            <div class="more-info-img">
-                                <img src="{{ asset('assets/images/s-video-3.png') }}" alt="">
-                            </div>
-                            <div class="more-info-title">
-                                <span>Dermatologie générale</span>
-                                <p>Démarche diagnostique des affections...</p>
-                            </div>
-                            <div class="more-info-date">
-                                <p>
-                                    Pour près de 20 000 professionnels de santé. <span>Mis à jour en 2024.</span>
-                                </p>
-                            </div>
-                        </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
@@ -67,143 +57,17 @@
                     style="-webkit-transform:translate3d(0, 70px, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-moz-transform:translate3d(0, 70px, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);-ms-transform:translate3d(0, 70px, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);transform:translate3d(0, 70px, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0);opacity:1"
                     class="class-details-instructor-inner">
                     <div class="class-details-instructor-image-wrap"><img
-                            src="https://www.santeacademie.com/_next/image?url=https%3A%2F%2Ffrontstage.santeacademie.com%2Fuploads%2Ftrainer-39de27%2F7%2F4%2Fpicture%2FprofilePicture%2FprofilePicture-user_avatar-Z9PSdT02we.png&w=640&q=75"
+                            src="{{ asset('storage/'.$doctor->avatar) }}"
                             loading="lazy" alt="Instructor Image" class="class-details-instructor-image" />
                     </div>
                     <div class="class-details-instructor-content-wrap">
-                        <div class="class-details-instructor-name">Dr Mathieu Bataille</div>
-                        <div class="class-details-instruction-designation">Médecin dermatologue</div>
-                        <p class="class-details-instructor-excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa dolor sunt eveniet possimus eligendi dolore cum iure alias. Doloribus, et?</p>
+                        <div class="class-details-instructor-name">{{ $doctor->name }}</div>
+                        <div class="class-details-instruction-designation">{{ $doctor->speciality }}</div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-{{-- <section class="py-12 bg-gray-50">
-    <div class="container mx-auto px-6 lg:px-20">
-        <div class="max-w-4xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
-            <div class="grid grid-cols-1 md:grid-cols-3">
-
-                <!-- Left Column: Image -->
-                <div class="md:col-span-1">
-                    <img src="{{ asset('storage/'.$doctor->avatar) }}" alt="{{ $doctor->name }}"
-                        class="w-full h-auto object-cover">
-                </div>
-
-                <!-- Right Column: Doctor Info -->
-                <div class="md:col-span-2 p-6">
-                    <div class="mb-4">
-                        <h1 class="text-2xl font-bold text-gray-800">{{ $doctor->name }}</h1>
-                        <p class="text-sm text-gray-500">Joined on {{ \Carbon\Carbon::parse($doctor->created_at)->format('d F Y') }}</p>
-                    </div>
-                    
-                    <div class="mb-4">
-                        <p class="text-lg text-gray-700"><strong>Bio: </strong>{{ $doctor->bio }}</p>
-                    </div>
-
-                    <div class="mb-4 space-y-1">
-                        <p class="text-gray-700"><strong>Email: </strong>
-                            <a href="mailto:{{ $doctor->email }}" class="text-blue-500 hover:underline">
-                                {{ $doctor->email }}
-                            </a>
-                        </p>
-                        <p class="text-gray-700"><strong>Phone: </strong>{{ $doctor->phone }}</p>
-                        @if ($doctor->speciality)
-                            <p class="text-gray-700"><strong>Speciality: </strong>{{ $doctor->speciality }}</p>
-                        @endif
-                    </div>
-
-                    <!-- Social Links -->
-                    <div class="flex space-x-4 mt-6">
-                        @if ($doctor->facebook)
-                            <a href="{{ $doctor->facebook }}" target="_blank"
-                                class="text-blue-600 hover:text-blue-800">
-                                <svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24">
-                                    <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 5.522 4.477 10 10 10s10-4.478 10-10zM9.775 15.367h1.694v-5.487h-1.694v-1.25h1.694V8.56c0-1.308.794-2.038 1.968-2.038.565 0 .976.048 1.222.07v1.4l-.89.005c-.697 0-.933.341-.933.928v.926h1.819l-.238 1.252h-1.58v5.483h-1.694v-5.482H9.775v5.487z" />
-                                </svg>
-                            </a>
-                        @endif
-                        @if ($doctor->linkedin)
-                            <a href="{{ $doctor->linkedin }}" target="_blank"
-                                class="text-blue-700 hover:text-blue-900">
-                                <svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24">
-                                    <path d="M19 0H5C2.25 0 0 2.25 0 5v14c0 2.75 2.25 5 5 5h14c2.75 0 5-2.25 5-5V5c0-2.75-2.25-5-5-5zM9.045 17.437h-2.99V8.573h2.99v8.865zm-1.485-10.07c-1.032 0-1.756-.765-1.756-1.767 0-.987.752-1.762 1.785-1.762 1.033 0 1.755.764 1.756 1.767-.003.988-.744 1.762-1.785 1.762zm11.935 10.07h-2.999v-4.765c0-1.128-.405-1.898-1.418-1.898-.773 0-1.234.52-1.435 1.02-.074.179-.093.429-.093.68v4.962h-2.993s.038-8.062 0-8.865h2.993v1.254c-.006.01-.015.02-.021.03h.021v-.03c.4-.617 1.114-1.5 2.712-1.5 1.978 0 3.463 1.3 3.463 4.095v5.016z" />
-                                </svg>
-                            </a>
-                        @endif
-                        @if ($doctor->youtube)
-                            <a href="{{ $doctor->youtube }}" target="_blank"
-                                class="text-red-600 hover:text-red-800">
-                                <svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24">
-                                    <path d="M23.499 6.203c-.28-1.036-1.048-1.83-2.044-2.101-1.805-.505-9.006-.505-9.006-.505s-7.202 0-9.007.505c-.996.271-1.763 1.064-2.043 2.101C.167 7.27.167 11.999.167 11.999s0 4.729.232 5.796c.28 1.037 1.048 1.83 2.043 2.101 1.806.505 9.007.505 9.007.505s7.201 0 9.006-.505c.996-.271 1.764-1.064 2.044-2.101.232-1.067.232-5.796.232-5.796s0-4.729-.232-5.796zM9.709 15.551V8.45l6.18 3.551-6.18 3.55z" />
-                                </svg>
-                            </a>
-                        @endif
-                        @if ($doctor->twitter)
-                            <a href="{{ $doctor->twitter }}" target="_blank"
-                                class="text-blue-400 hover:text-blue-600">
-                                aa
-                                <svg class="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 24 24">
-                                    <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-.25 16.5c-2.698 0-5.5-1.198-5.5-2.75 0-1.562 2.802-2.75 5.5-2.75 2.698 0 5.5 1.188 5.5 2.75 0 1.562-2.802 2.75-5.5 2.75zm-4.5-6.25c-.969 0-1.75-.785-1.75-1.75s.781-1.75 1.75-1.75 1.75.785 1.75 1.75-.781 1.75-1.75 1.75zm7.25.25c-1.484 0-2.75-.988-2.75-2.25s1.266-2.25 2.75-2.25 2.75.988 2.75 2.25-1.266 2.25-2.75 2.25z" />
-                                </svg>
-                            </a>
-                        @endif
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section> --}}
-
-
-
-
-<hr>
-
-<!-- Doctor's Formations Section -->
-{{-- <section class="py-12 bg-gray-50">
-    <div class="container mx-auto px-6 lg:px-20">
-        <h2 class="text-2xl font-bold text-gray-800 mb-6">Formations de {{ $doctor->name }}</h2>
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-            @forelse($doctor->formations as $formation)
-                <div class="bg-white shadow-lg rounded-lg overflow-hidden">
-                    <a href="{{ route('frontend.formation', $formation->id) }}">
-                        <img src="{{ asset('storage/' . $formation->cover) }}" alt="{{ $formation->title }}"
-                            class="w-full h-48 object-cover">
-                    </a>
-                    <div class="p-4">
-                        <!-- Title -->
-                        <h3 class="text-lg font-semibold mb-2">
-                            <a href="{{ route('frontend.formation', $formation->id) }}"
-                                class="hover:underline text-gray-800">
-                                {{ $formation->title }}
-                            </a>
-                        </h3>
-                        <!-- Category and Date -->
-                        <div class="text-sm text-gray-500">
-                            <span>{{ $formation->category->name }}</span>
-                            <span class="mx-2">|</span>
-                            <span>{{ \Carbon\Carbon::parse($formation->date)->format('d F Y') }}</span>
-                        </div>
-                        <!-- Button -->
-                        <a href="{{ route('frontend.formation', $formation->id) }}"
-                            class="mt-4 inline-block text-sm bg-blue-600 text-white px-4 py-2 rounded shadow hover:bg-blue-700">
-                            Voir Plus
-                        </a>
-                    </div>
-                </div>
-            @empty
-                <p class="text-gray-700">Aucune formation pour l'instant.</p>
-            @endforelse
-        </div>
-    </div>
-</section> --}}
 
 @endsection
