d4f0491b6c4d876425266aa2db6bbc854c8bf2c4
marwanBenz <benzsram@gmail.com>
2024-12-28 22:50:48 +0100

    Finish top bar design

--- resources/views/livewire/top-bar.blade.php
+++ resources/views/livewire/top-bar.blade.php
@@ -1,8 +1,21 @@
 <div class="top-bar-wrapper">
     <div class="w-layout-blockcontainer container w-container">
         <div class="top-bar">
-            <a href="{{ route('frontend.index') }}" class="navbar-brand w-nav-brand"><img
-            loading="lazy" src="{{ asset('storage/'.setting('site.logo')) }}" alt="Brand Logo" class="brand-logo" /></a>
+            <a href="{{ route('frontend.index') }}" class="navbar-brand w-nav-brand">
+                <img
+                loading="lazy" src="{{ asset('storage/'.setting('site.logo')) }}" alt="Brand Logo" class="brand-logo" />
+            </a>
+            <div class="top-bar-right">
+                <div class="search-bar">
+                    <input type="text" placeholder="Chercher votre future formation">
+                </div>
+                <a href="">
+                    <div class="call-btn">
+                        <img src="{{ asset('assets/svg/call.svg') }}" alt="">
+                        <span>Appeler un conseiller</span>
+                    </div>
+                </a>
+            </div>
         </div>
     </div>
 </div>
