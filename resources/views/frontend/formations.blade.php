c4ebb4e06e2365ae32ae5a98129b053f93bd9068
marwanBenz <benzsram@gmail.com>
2025-01-07 19:02:50 +0100

    Rechange all formation heading content

--- resources/views/frontend/formations.blade.php
+++ resources/views/frontend/formations.blade.php
@@ -5,8 +5,8 @@
 <div class="formation-header section-padding-top-50">
     <div class="w-layout-blockcontainer container w-container">
         <div class="all-formation-heading">
-            La formation <span>Nephrologue</span>, classe virtuelle et e-learning vidéo
-            <p>Apprenez les bases de la <span>néphrologie</span> à travers des cours en ligne et des vidéos</p>
+            Formation en ligne, classe virtuelle et e-learning vidéo
+            <p>Apprenez les bases à travers des cours en ligne et des vidéos</p>
         </div>
     </div>
 </div>
